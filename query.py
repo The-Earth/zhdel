ca06f449bf679b1abe8c1c6afac0c1f752f2abd6
Misaka10032 <tigerzeng97@gmail.com>
2018-02-04 11:57:12 +0800

    WP:已删除内容查询

---
+++ query.py
@@ -0,0 +1,25 @@
+﻿import mwclient
+import os
+
+zh = mwclient.Site('zh.wikipedia.org')
+dp = mwclient.Site('zhdel.miraheze.org')
+
+zh.login('','')
+
+title = input('Page:')
+dpp = dp.Pages[title]
+
+if dpp.exists:
+    text = dpp.text()
+    print(text)
+    edit=int(input('是否搬运？'))
+    if edit:
+        zhp = zh.Pages['User:Tigerzeng/zhdel/' + title]
+        zhp.save(text, '查询已删除内容')
+        revid = next(zhp.revisions())['revid']
+        zhp.save('{{User:Tigerzeng/zhdel/header|title='+title+'|id='+str(revid)+'}}', '查询已删内容')
+
+else:
+    print('页面未收录')
+
+os.system('pause')
