5ca281eec90caa1ac37ac1a1f0d4dffe991704fa
The-Earth <tigerzeng97@gmail.com>
2018-06-23 17:00:00 +0800

    replace tab with space & satisfy BY requirements

--- zhdel.py
+++ zhdel.py
@@ -13,76 +13,78 @@ dp.login('','') #Bot user name & password
 print('Login successfully!')
 
 def fetch(title):
-	sta = status(title)
-	print('Checking',title,':',sta)
-	if sta == 'update' or sta == 'new':
-		page = zh.Pages[title]
-		txt = page.text()
-		new = dp.Pages[title]
-		if sta == 'update':
-			try:
-				new.save(txt,'Bot: Page updated.')
-			except EditError:
-				revoke()	#revoke auto-confirmed
-				new.save(txt,'Bot: Page updated.')	#retry
-		elif sta == 'new':
-			try:
-				new.save(txt,'Bot: New page collected.')
-				with open(logdir,'a') as log:
-					log.write(title+'\n')
-			except EditError:
-				revoke()	#revoke auto-confirmed
-				new.save(txt,'Bot: New page collected.')	#retry
-				with open(logdir,'a') as log:
-					log.write(title+'\n')
+    sta = status(title)
+    print('Checking',title,':',sta)
+    if sta == 'update' or sta == 'new':
+        page = zh.Pages[title]
+        rev = next(page.revisions())
+        by = '由[[w:zh:User:%s|%s]]于%s年%s月%s日 %s:%s:%s(UTC)做出的[[w:zh:Special:permalink/%s|版本]]，编辑摘要：%s' % (rev['user'],rev['user'],rev['timestamp'].tm_year,rev['timestamp'].tm_mon,rev['timestamp'].tm_mday,rev['timestamp'].tm_hour,rev['timestamp'].tm_min,rev['timestamp'].tm_sec,rev['revid'],rev['comment'])
+        new = dp.Pages[title]
+        txt = by + '\n' + page.text()
+        if sta == 'update':
+            try:
+                new.save(txt,'Bot: Page updated.')
+            except EditError:
+                revoke()	#revoke auto-confirmed
+                new.save(txt,'Bot: Page updated.')	#retry
+        elif sta == 'new':
+            try:
+                new.save(txt,'Bot: New page collected.')
+                with open(logdir,'a') as log:
+                    log.write(title+'\n')
+            except EditError:
+                revoke()	#revoke auto-confirmed
+                new.save(txt,'Bot: New page collected.')	#retry
+                with open(logdir,'a') as log:
+                    log.write(title+'\n')
 
-	elif sta == 'deleted':
-		pass
-	elif sta == 'nobot':
-		pass
-	elif sta == 'well':
-		pass
+    elif sta == 'deleted':
+        pass
+    elif sta == 'nobot':
+        pass
+    elif sta == 'well':
+        pass
 
 def revoke():
-	print('Autoconfirmed, revoke it.')
-	talk = dp.Pages['User talk:Tiger-bot']
-	try:
-		talk.save('!revoke!')
-	except EditError:
-		pass
+    print('Autoconfirmed, revoke it.')
+    talk = dp.Pages['User talk:Tiger-bot']
+    try:
+        talk.save('!revoke!')
+    except EditError:
+        pass
 
 def status(title):
-	wpp = zh.Pages[title]
-	wpt = wpp.text()
-	dpp = dp.Pages[title]
-	dpt = dpp.text()
-	loglist = list(open(logdir))
-	if not title+'\n' in loglist:
-		return 'new'
-	if not wpp.exists:
-		return 'deleted'
-	if wpt != dpt:
-		return 'update'
-	if '!nobot!' in dpt:
-		return 'nobot'
-	else:
-		return 'well'
+    wpp = zh.Pages[title]
+    wpt = wpp.text()
+    dpp = dp.Pages[title]
+    dpt = dpp.text()
+    loglist = list(open(logdir))
+    if not title+'\n' in loglist:
+        return 'new'
+    if not wpp.exists:
+        return 'deleted'
+    if wpt != dpt:
+        return 'update'
+    if '!nobot!' in dpt:
+        return 'nobot'
+    else:
+        return 'well'
 
 def main():
-	for nom in zh.search(r'insource:/\{\{\s*((db|d|sd|csd|speedy|delete|速刪|速删|快刪|快删|有爭議|有争议|[vaictumr]fd|delrev|存廢覆核|存废复核)\s*(\||}})|(db|vfd)-)/'):
-		fetch(nom['title'])
+    for nom in zh.search(r'insource:/\{\{\s*((db|d|sd|csd|speedy|delete|速刪|速删|快刪|快删|有爭議|有争议|[vaictumr]fd|delrev|存廢覆核|存废复核)\s*(\||}})|(db|vfd)-)/'):
+        fetch(nom['title'])
 
 def kill():
-	talk = dp.Pages['']     #Bot control page
-	talktxt = talk.text()
-	if '!stop!' in  talktxt:
-		return True
-	else:
-		return False
+    talk = dp.Pages['']     #Bot control page
+    talktxt = talk.text()
+    if '!stop!' in  talktxt:
+        return True
+    else:
+        return False
 
 while True:
-	if kill():
-		break
-	else:
-		main()
-		time.sleep(60)
+    if kill():
+        break
+    else:
+        main()
+        time.sleep(60)
